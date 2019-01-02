ad023b89699de01b91a383ba797251018269227e
koryo93 <koryo93@gmail.com>
2019-01-02 20:06:06 +0900

    리스트

---
+++ board/bbs_1/list.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+<link type="text/css" href="../../lib/m_style.css" rel='stylesheet' />
+<title>TEST 게시판</title>
+</head>
+
+<body>
+<TABLE BORDER='0' CELLSPACING='0' CELLPADDING='0' WIDTH='100%' HEIGHT='100%' ALIGN='CENTER' VALIGN='TOP'>
+<TR>
+<TD WIDTH='100%'  HEIGHT='70'  ALIGN='LEFT'  VALIGN='MIDDLE' BGCOLOR='#E89C05'>
+<table border='0' width='90%' height='70' bgcolor='#E89C05' align='center' cellspacing='0' cellpadding='0'>  
+<tr> 
+    <td width='100%' height='70' align='left' valign='middle'>
+	<a href='../../index.php'><strong>[홈]</strong></a>
+	 &nbsp; &nbsp; &nbsp;
+	 <a href='./write.php'><strong>[게시판 글쓰기]</strong></a>
+</td>
+
+<tr> 
+    <td width='100%' height='100%' align='left' valign='middle'>&nbsp;</td>
+
+	</tr>
+	</table>
+
+</TD>
+
+
+<TR>
+<TD WIDTH='100%'  HEIGHT='100%'  ALIGN='CENTER'  VALIGN='TOP'>&nbsp;</TD>
+</TR>
+</TABLE>
+
+</body>
+</html>
