9c577f990ecfb1ddf0c9f8c153ff57497f2e5e95
koryo93 <koryo93@gmail.com>
2019-01-04 23:39:13 +0900

    쓰기 처리

--- board/bbs_1/write_post.php
+++ board/bbs_1/write_post.php
@@ -1,41 +1,31 @@
-<?header("content-type:text/html; charset=UTF-8");
+<?php
+header("content-type:text/html; charset=UTF-8");
 
-/**************************************************************
-***************************************************************
-PHP Script Produced by: 박정원 PJW(Jimmy)  
-Email: pjws0321@naver.com / pjws0321@gmail.com 
-The purpose of the product : Educational (교육용)
-Notice: 본 스크립트는 교육용도로 만들어 졌습니다. 
-Blog  http://blog.naver.com/pjws0321
-****************************************************************
-***************************************************************/
-
- $id= $_POST[id];
- $user_id=$_POST[user_id];
- $name=$_POST[name];
- $pw=$_POST[pw];
- $memo=$_POST[memo];
+$id = $_POST["id"];
+$user_id = $_POST["user_id"];
+$name = $_POST["name"];
+$pw = $_POST["pw"];
+$memo = $_POST["memo"];
  
- $regdate=date("YmdHis", time()); //날짜 , 시간
- $ip=getenv("REMOTE_ADDR"); //ip
+$regdate = date("YmdHis", time()); //날짜 , 시간
+$ip = getenv("REMOTE_ADDR"); //ip
 
- $connect= mysql_connect("localhost","jungwon","jw12345"); //mysql 연결
- mysql_select_db("jungwon",$connect); //DB선택
- if(!$connect){
-	 echo "연결에 실패 하였습니다.".mysql_error();
- }
-
+$connect = mysqli_connect("localhost","root","88deluxe", "board"); //mysql 연결
+if( !$connect )
+{
+ echo "연결에 실패 하였습니다.".mysqli_error();
+}
 
 //쿼리전송
-$query="insert into bbs_1(id, user_id, name, pw, memo, regdate, ip) 
-                   values('$id','$user_id','$name','$pw','$memo','$regdate','$ip')";
-mysql_query("set names utf8", $connect);
-mysql_query($query, $connect);
+$query = "INSERT INTO 
+            bbs_1(id, user_id, name, pw, memo, regdate, ip) 
+          VALUES('$id','$user_id','$name','$pw','$memo','$regdate','$ip')";
+mysqli_query($connect, $query);
 
-mysql_close; //끝내기.
+mysqli_close($connect); //끝내기.
 ?>
 
 <script>
-window. alert('쿼리가 정상적으로 전송 되었습니다.');
-location.href='./list.php';
+    window. alert('쿼리가 정상적으로 전송 되었습니다.');
+    location.href='./list.php';
 </script>
